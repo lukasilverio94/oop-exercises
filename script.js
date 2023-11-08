a4b753b787d191f3e730136279d244606cb08a6b
lukasilverio94 <lukasilverio94@gmail.com>
2023-11-08 11:52:19 +0100

    added render UI funcionality

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Exercise 1 - Classes
-// Create a class called `Person` with the following properties and methods:
-// 1 - Properties:
-//  `name` (string)
-//  `age` (number)
-// 2 - Methods:
-//  `introduce()`: This method should return a string introducing the person, for example: "Hi, my name is John and I am 25 years old."
+const btnPerson = document.getElementById("btn-person");
+const showPerson = document.getElementById("show-person");
+const btnPromise = document.getElementById("btn-promise");
+const showPromise = document.getElementById("show-promise");
+console.log(btnPromise, showPromise);
+
+console.log(showPerson);
 class Person {
   constructor(name, age) {
     this.name = name;
@@ -25,6 +26,10 @@ const describePerson = (callback) => {
   return callback.call(person1);
 };
 
+// Event listener Introduce Person.
+btnPerson.addEventListener("click", () => {
+  showPerson.innerText = person1.introduce();
+});
 // Exercise 3 - Promises
 // Write a function called `wait` that takes a number as an argument (representing milliseconds) and returns a Promise. The Promise should resolve after the given number of milliseconds, using `setTimeout`. The resolved value can be anything you choose.
 const wait = (milliseconds) => {
@@ -37,15 +42,15 @@ const wait = (milliseconds) => {
 };
 
 //Using promise
-wait(3000) //Call function and pass milliseconds as param.
-  .then((result) => {
-    console.log(result); //If resolve, return this result
-  })
-  .catch((error) => {
-    console.log(error); //otherwise, throw error.
-  });
+// Event Listener to Promise
+btnPromise.addEventListener("click", () => {
+  showPromise.innerText = "Waiting...";
 
-//TASK 4 Putting it all together
-//Introduction to console:
-const introduction = describePerson(person1.introduce);
-console.log(introduction);
+  wait(2000) //Call function and pass milliseconds as param.
+    .then((result) => {
+      showPromise.innerText = result; //If resolve, return this result
+    })
+    .catch((error) => {
+      showPromise.innerText = error; //otherwise, throw error.
+    });
+});
